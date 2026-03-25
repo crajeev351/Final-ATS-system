cfa5052ae0a1b6c9152013870dd744726991cca9
Rajeev <crajeev351@gmail.com>
2026-03-25 11:55:34 +0530

    Initial project

---
+++ main.py
@@ -0,0 +1,39 @@
+from flask import Flask, render_template, request, redirect, url_for
+
+app = Flask(__name__)
+
+users = {}   # temporary storage
+
+# DEFAULT PAGE → SIGNUP
+@app.route('/', methods=['GET', 'POST'])
+def signup():
+    message = ""
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+
+        if username in users:
+            message = "User already exists!"
+        else:
+            users[username] = password
+            return redirect(url_for('login'))
+
+    return render_template('signup.html', message=message)
+
+# LOGIN PAGE
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    message = ""
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+
+        if username in users and users[username] == password:
+            message = "Login Successful!"
+        else:
+            message = "Invalid Credentials"
+
+    return render_template('login.html', message=message)
+
+if __name__ == '__main__':
+    app.run(debug=True)
